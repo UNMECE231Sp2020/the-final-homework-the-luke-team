2e697977c8c50af4417ecb33de0817bb0deb1dbf
fviramontes8 <kuolemajarvi@yahoo.com>
2020-04-28 23:43:42 -0700

    Added more comments

--- GeneralQueue.hpp
+++ GeneralQueue.hpp
@@ -12,6 +12,7 @@ class Queue {
 	public:
 		//Default constructor
 		Queue() {
+			//TODO: _data is not a pointer!
 			_data = nullptr;
 		//	_size = _data.size();
 			_size = 0;
@@ -19,50 +20,52 @@ class Queue {
 
 
 		//Copy constructor
+		//TODO: Change List<Q> to Queue
 		Queue(const List<Q> &q){ //List<Q> to Queue
 			_data = q._data;
 			_size = q._size; //_size to .size()
+		}
 
 		//Getters
-		size_t size() const{
+		size_t size() const {
 			return _size;
 		}
 
 
-		Q front() const{
+		Q front() const {
 			return _data.front();
 		}
 
 
-		Q back() const{
+		Q back() const {
 			return _data.back();
 		}
 
 
 		//Push to queue
-		void enqueue(Q value){
+		void enqueue(Q value) {
 			_data.push_front(value);
 			_size = _data.size();
 		}
 
 
 		//Pop from queue
-		void dequeue(){
+		void dequeue() {
 			_data.pop_back();
 			_size = _data.size();
 		}
 
-		void print(){
+		void print() {
 			_data.print();
 		}
 
 
-		bool search(Q value){
+		bool search(Q value) {
 			return _data.search(value);
 		}
 
 
-		bool empty(){
+		bool empty() {
 			return _data.empty();
 		}
 
